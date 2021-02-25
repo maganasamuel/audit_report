d1023fddd1dc8977c939724793b8f49e3020f23a
Andre Demavivas <andredemavivas@gmail.com>
2021-02-25 11:57:43 +0800

    test audit

--- resources/views/calls/audit.blade.php
+++ resources/views/calls/audit.blade.php
@@ -13,45 +13,46 @@
           <div class="card-title">
             <h3 class="mb-0">Audit Report</h3>
           </div>
-            <div class="assessment-container container">
+            <div class="assessment-container">
                 <div class="row">
-                    <div class="col-lg-12 pt-0 form-box">
+                    <div class="col-lg-12 w-100 pt-0 form-box">
                         <form role="form" class="registration-form" action="javascript:void(0);">
                             <fieldset>
                                 <div class="form-top">
                                 </div>
                                 <div class="form-bottom">
                                     <div class="row">
-                                        <div class="form-group col-md-6 col-sm-6">
-                                            <input type="text" class="form-control" placeholder="Week of" id="fname">
+                                        <div class="form-group col-lg-6 col-md-12">
+                                            <input type="text" class="form-control" placeholder="Week of" id="week-of" value="{{ date('d/m/Y') }}">
                                         </div>
-                                        <div class="form-group col-md-6 col-sm-6">
+                                        <div class="form-group col-lg-6 col-md-12">
                                             <input type="text" class="form-control" placeholder="Lastname" id="lname">
                                         </div>
                                     </div>
                                     <div class="form-group" style="margin-bottom:3px;">
                                         <div class="row">
                                             <div class="form-group col-lg-6 col-md-12">
-                                                <select class="form-control">
+                                                <select class="form-control" name="adviser" id="adviser" required>
                                                     <option value="" selected disabled>Select an Adviser</option>
                                                 </select>
                                             </div>
                                             <div class="form-group col-lg-6 col-md-12">
-                                                <input type="text" class="form-control" placeholder="Lead Source" id="lead_source">
+                                              <select class="form-control" name="lead_source" id="lead-source" required>
+                                                <option value="" selected disabled>Select an Lead Source</option>
+                                              </select>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <input type="text" name="email" placeholder="Email" class="form-email form-control" id="email" required>
+                                    <div class="row">
+                                      <div class="form-group col-lg-6 col-md-12">
+                                          <input type="text" name="policy_holder" placeholder="Policy Holder" class="form-email form-control" id="policy-holder" required>
+                                      </div>
+                                      <div class="form-group col-lg-6 col-md-12">
+                                          <input type="text" name="policy_no" id="policy-no" placeholder="Policy No" class="form-control" required>
+                                      </div>
+                                      
                                     </div>
 
-                                    <div class="form-group">
-                                        <select class="form-control">
-                                            <option>Gender</option>
-                                            <option>Male</option>
-                                            <option>Female</option>
-                                        </select>
-                                    </div>
                                     
                                     <button type="button" class="btn btn-next">Next</button>
                                 </div>
