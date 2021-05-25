2b60c19b662faf56de7fb24e4eb559d7f00b636b
Samuel Magana <maganasamuel001@gmail.com>
2021-05-26 11:26:18 +1200

    Only hide alert component

--- resources/views/alerts/success.blade.php
+++ resources/views/alerts/success.blade.php
@@ -1,7 +1,17 @@
 <div class="alert alert-success alert-with-icon d-none" id="success">
-    <a href="#" aria-hidden="true" class="close" data-dismiss="alert" aria-label="close">
-      &times;
-    </a>
-    <span data-notify="icon" class="tim-icons icon-trophy"></span>
-    <span id="success-text"></span>
+  <a href="javascript:void(0)" aria-hidden="true" class="close"
+    aria-label="close">
+    &times;
+  </a>
+  <span data-notify="icon" class="tim-icons icon-trophy"></span>
+  <span id="success-text"></span>
 </div>
+
+@push('scripts')
+  <script type="text/javascript">
+    $('#success').on('click', function() {
+      $(this).removeClass('d-block').addClass('d-none');
+    })
+
+  </script>
+@endpush
