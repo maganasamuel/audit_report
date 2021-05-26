2d4f8abce9dfc4ad00089bdd4b1d28166d915797
Samuel Magana <maganasamuel001@gmail.com>
2021-05-26 13:44:50 +1200

    Cast qa column to array

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -11,28 +11,34 @@ class Audit extends Model
 
     protected $guarded = [];
 
+    protected $casts = [
+        'qa' => 'array',
+    ];
 
-    public function clients(){
-      return $this->belongsToMany(Client::class)
-                  ->withPivot('weekOf', 'lead_source', 'pdf_title')
-                  ->withTimestamps();
+    public function clients()
+    {
+        return $this->belongsToMany(Client::class)
+            ->withPivot('weekOf', 'lead_source', 'pdf_title')
+            ->withTimestamps();
     }
 
-    public function advisers(){
-      return $this->hasMany(Adviser::class);    
+    public function advisers()
+    {
+        return $this->hasMany(Adviser::class);
     }
 
     public function adviser()
     {
-      return $this->belongsTo(Adviser::class);
+        return $this->belongsTo(Adviser::class);
     }
 
-    public function users(){
-      return $this->belongsTo(User::class);
+    public function users()
+    {
+        return $this->belongsTo(User::class);
     }
 
-    public function caller() {
-
-      return $this->hasOne(User::class, 'id', 'user_id');
+    public function caller()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
     }
 }
