8b5e694053b4224c4f60de56d3a7c38fb7184862
Samuel Magana <maganasamuel001@gmail.com>
2021-05-28 16:06:21 +1200

    Removed docblock

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class AuditController extends Controller
 {
-    /**
-     * Display listing of the resource
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('audits.create');
