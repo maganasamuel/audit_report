b6208599e81186f5f341212714ba53184a4f6258
Samuel Magana <maganasamuel001@gmail.com>
2021-05-29 16:01:13 +1200

    Created mail for audit report

---
+++ app/Mail/AuditMail.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use niklasravnsborg\LaravelPdf\Facades\Pdf;
+
+class AuditMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $audit;
+
+    public $policy_holder;
+
+    public $policy_no;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param mixed $audit
+     *
+     * @return void
+     */
+    public function __construct($audit)
+    {
+        $this->audit = $audit;
+
+        $this->policy_holder = $this->audit->client->policy_holder;
+        $this->policy_no = $this->audit->client->policy_no;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $pdf = Pdf::loadView('pdfs.audit-mail', [
+            'audit' => $this->audit,
+        ]);
+
+        return $this->markdown('emails.audit')
+            ->subject('Audit Report')
+            ->attachData($pdf->output(), 'audit-report.pdf');
+
+    }
+}
