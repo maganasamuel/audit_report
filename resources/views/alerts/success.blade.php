3e107c0d8da0e7784a257137da1e02a2b1e35c97
Samuel Magana <maganasamuel001@gmail.com>
2021-05-30 13:34:25 +1200

    Refactor alert success javascript

--- resources/views/alerts/success.blade.php
+++ resources/views/alerts/success.blade.php
@@ -1,21 +1,9 @@
 <div class="alert alert-success alert-with-icon d-none" id="success">
-  <a href="javascript:void(0)" aria-hidden="true" class="close"
-    aria-label="close">
+  <button type="button" aria-hidden="true" class="close"
+    aria-label="close"
+    onclick="let success = this.closest('#success'); success.classList.remove('d-block'); success.classList.add('d-none');">
     &times;
-  </a>
+  </button>
   <span data-notify="icon" class="tim-icons icon-trophy"></span>
   <span id="success-text"></span>
 </div>
-
-@push('scripts')
-  <script type="text/javascript">
-    window.onload = () => {
-      $(function() {
-        $('#success').on('click', function() {
-          $(this).removeClass('d-block').addClass('d-none');
-        });
-      });
-    }
-
-  </script>
-@endpush
