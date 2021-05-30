362269a653f3d1e7f227521848f2912eefac43d2
Samuel Magana <maganasamuel001@gmail.com>
2021-05-30 20:25:27 +1200

    Created blade view for editing survey

---
+++ resources/views/surveys/edit.blade.php
@@ -0,0 +1,22 @@
+<div>
+  <div class="modal right fade" id="editSurveyModal"
+    tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            {{ __('Edit Survey') }}</h5>
+          <button type="button" class="close" data-dismiss="modal"
+            aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+
+        <div class="modal-body">
+          @livewire('surveys.form', ['profileClientId' => $clientId])
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
