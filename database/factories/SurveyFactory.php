02a33a0831d3e9ca9571ee380b152ce136243233
Samuel Magana <maganasamuel001@gmail.com>
2021-05-30 22:18:38 +1200

    Use survey questions config

--- database/factories/SurveyFactory.php
+++ database/factories/SurveyFactory.php
@@ -24,37 +24,24 @@ class SurveyFactory extends Factory
      */
     public function definition()
     {
+        $sa = [];
+
+        foreach (config('services.survey.questions') as $key => $question) {
+            if (in_array($question['type'], ['text', 'text-optional'])) {
+                $sa[$key] = $this->faker->catchPhrase;
+            } elseif ('boolean' == $question['type']) {
+                $sa[$key] = 'yes';
+            } elseif ('select' == $question['type']) {
+                $sa[$key] = $this->faker->randomElement(collect($question['values'])->pluck('value')->all());
+            }
+        }
+
         return [
             'adviser_id' => Adviser::count() ? Adviser::inRandomOrder()->first()->id : Adviser::factory(),
             'client_id' => Client::count() ? Client::inRandomOrder()->first()->id : Client::factory(),
             'created_by' => User::count() ? User::inRandomOrder()->first()->id : User::factory(),
             'updated_by' => User::count() ? User::inRandomOrder()->first()->id : User::factory(),
-            'sa' => [
-                'answers' => [
-                    'Yes',
-                    'Alice',
-                    'Yes',
-                    'Yes',
-                    'Yes',
-                    'Yes',
-                    'Test cancel',
-                    'Igor India',
-                    'Test improve',
-                ],
-                'questions' => [
-                    'Have you had a chance to discuss this cancellation with your Adviser?',
-                    'Who is your Adviser?',
-                    'Are you replacing your Partners Life Policy with one at another Provider?',
-                    'Did your Adviser explain the differences between your Partners Life Policy and your new replacement insurance Policy?',
-                    'Did your Adviser explain the risk of Non-Disclosure or Misstatement to you?',
-                    'Did your Adviser discuss both the benefits you forfeit and any risks you might be exposed to in cancelling your cover from us?',
-                    'Why are you cancelling your Policy with us?',
-                    'Who is your new insurer?',
-                    'What could we do to improve?',
-                ],
-            ],
-            'created_by' => User::count() ? User::inRandomOrder()->first()->id : User::factory(),
-            'updated_by' => User::count() ? User::inRandomOrder()->first()->id : User::factory(),
+            'sa' => $sa,
         ];
     }
 }
