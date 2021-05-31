011b6be22cdaabffe54d2f51de439e133ac76813
Samuel Magana <maganasamuel001@gmail.com>
2021-05-31 16:23:01 +1200

    Used livewire components for user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,14 +1,22 @@
 @extends('layouts.app', ['title' => 'Users'])
 
 @section('content')
-  @include('users.partials.header', ['title' => 'Users'])
+  @include('users.partials.header', ['title' => 'Users', 'description' => ''])
 
   <div class="container-fluid mt--7">
     @include('alerts.success')
     @include('alerts.error')
 
-    <div class="card">
-      <div class="card-body"></div>
+    <div class="card w-100">
+      <div class="card-body">
+        <div class="card-title d-flex">
+          <div class="button-container">
+            @livewire('users.form')
+          </div>
+        </div>
+
+        @livewire('users.index')
+      </div>
     </div>
 
     @include('layouts.footers.auth')
