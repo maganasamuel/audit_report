cde87dae577ca5b0614caa1da05e1df88d59c917
Samuel Magana <maganasamuel001@gmail.com>
2021-09-07 02:23:00 +1200

    Updated Test Seeder

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Adviser;
 use App\Models\Audit;
 use App\Models\Survey;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TestSeeder extends Seeder
 {
@@ -16,6 +18,10 @@ class TestSeeder extends Seeder
      */
     public function run()
     {
+        $this->training();
+
+        return;
+
         User::factory()->create([
             'email' => 'inactive-user@mail.com',
             'status' => 'Deactivated',
@@ -26,5 +32,16 @@ class TestSeeder extends Seeder
 
         Audit::factory(200)->create();
         Survey::factory(200)->create();
+    }
+
+    public function training()
+    {
+        $audits = Audit::leftJoin(config('database.connections.mysql_training.database') . '.ta_user as adviser', 'audits.adviser_id', '=', 'adviser.id_user')
+            ->select([
+                'audits.*',
+                DB::raw('concat(adviser.first_name, " ", adviser.last_name) as adviser_name'),
+            ])->limit(5)->get();
+
+        dd($audits->toArray());
     }
 }
