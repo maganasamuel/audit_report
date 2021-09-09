4d58edbd9ecdae79e6805c4366d291650ebf974f
Samuel Magana <maganasamuel001@gmail.com>
2021-09-09 20:43:40 +1200

    Delete duplicates

--- database/seeders/MigrateTransactionsToTrainingSeeder.php
+++ database/seeders/MigrateTransactionsToTrainingSeeder.php
@@ -16,6 +16,8 @@ class MigrateTransactionsToTrainingSeeder extends Seeder
      */
     public function run()
     {
+        $this->fixDuplicates();
+
         $advisers = Audit::groupBy('adviser_id')->pluck('adviser_id')->mapWithKeys(function ($adviserId) {
             $email = DB::table('advisers')->where('id', $adviserId)->first()->email;
 
@@ -50,9 +52,11 @@ class MigrateTransactionsToTrainingSeeder extends Seeder
             ]];
         });
 
-        Log::info($advisers);
+        /* Log::info($advisers);
 
         Log::info($users);
+
+        return; */
 
         foreach ($advisers as $adviser_id => $adviser) {
             Audit::where('adviser_id', $adviser_id)->update([
@@ -68,6 +72,113 @@ class MigrateTransactionsToTrainingSeeder extends Seeder
             Audit::where('updated_by', $user_id)->update([
                 'updated_by' => $user['id'],
             ]);
+        }
+    }
+
+    public function fixDuplicates()
+    {
+        $trainingAttendees = explode(
+            ',',
+            DB::connection('mysql_training')
+                ->table('ta_training')
+                ->pluck('training_attendee')
+                ->implode(',')
+        );
+
+        $users = DB::connection('mysql_training')
+            ->table('ta_user')->select([
+                DB::raw('max(id_user) as id_user_max'),
+                'email_address',
+                DB::raw('count(id_user) as duplicate_role_count'),
+                DB::raw('group_concat(id_user_type order by id_user desc) as roles'),
+                DB::raw('group_concat(id_user order by id_user desc) as ids'),
+            ])->groupBy('email_address', 'id_user_type')
+            ->having('duplicate_role_count', '>', 1)
+            ->orderBy('id_user_max', 'desc')
+            ->get();
+
+        foreach ($users as $user) {
+            $ids = explode(',', $user->ids);
+
+            $user->id_user = array_shift($ids);
+
+            $user->duplicate_ids = $ids;
+
+            $user->duplicate_test_set_access = DB::connection('mysql_training')
+                ->table('ta_trainer_test_set_access')
+                ->whereIn('user_id', $user->duplicate_ids)
+                ->groupBy('user_id')
+                ->count();
+
+            $user->duplicate_test = DB::connection('mysql_training')
+                ->table('ta_test')
+                ->whereIn('id_user_tested', $user->duplicate_ids)
+                ->groupBy('id_user_tested')
+                ->count();
+
+            $user->duplicate_training_trainer = DB::connection('mysql_training')
+                ->table('ta_training')
+                ->whereIn('trainer_id', $user->duplicate_ids)
+                ->groupBy('trainer_id')
+                ->count();
+
+            $user->duplicate_training_training_attendee = collect($trainingAttendees)->filter(function ($item) use ($user) {
+                return in_array($item, $user->duplicate_ids);
+            })->count();
+        }
+
+        collect($users)->where('duplicate_test_set_access', '>', 0)->each(function ($user) {
+            DB::connection('mysql_training')
+                ->table('ta_trainer_test_set_access')
+                ->whereIn('user_id', $user->duplicate_ids)
+                ->update([
+                    'user_id' => $user->id_user,
+                ]);
+        });
+
+        collect($users)->where('duplicate_test', '>', 0)->each(function ($user) {
+            DB::connection('mysql_training')
+                ->table('ta_test')
+                ->whereIn('id_user_tested', $user->duplicate_ids)
+                ->update([
+                    'id_user_tested' => $user->id_user,
+                ]);
+        });
+
+        collect($users)->where('duplicate_training_trainer', '>', 0)->each(function ($user) {
+            DB::connection('mysql_training')
+                ->table('ta_training')
+                ->whereIn('training_id', $user->duplicate_ids)
+                ->update([
+                    'training_id' => $user->id_user,
+                ]);
+        });
+
+        collect($users)->where('duplicate_training_training_attendee', '>', 0)->each(function ($user) {
+            $trainings = DB::connection('mysql_training')
+                ->table('ta_training')
+                ->get();
+
+            foreach ($trainings as $training) {
+                $attendee = collect(explode(',', $training->training_attendee))->map(function ($item) use ($user) {
+                    return in_array($item, $user->duplicate_ids) ? $user->id_user : $item;
+                })->implode(',');
+
+                DB::connection('mysql_training')
+                    ->table('ta_training')
+                    ->where('training_id', $training->training_id)
+                    ->update([
+                        'training_attendee' => $attendee,
+                        'attendee_id' => $attendee,
+                    ]);
+            }
+        });
+
+        foreach ($users as $user) {
+            DB::connection('mysql_training')
+                ->table('ta_user')
+                ->whereIn('id_user', $user->duplicate_ids)
+                ->delete();
         }
     }
 }
